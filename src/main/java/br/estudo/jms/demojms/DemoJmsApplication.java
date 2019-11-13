dd04f3d842abb8bc91d182286825c1f227096eec
Ualison Aguiar <ualison.aguiar@gmail.com>
2019-11-13 07:47:27 -0300

    iniciando o estudo jms

---
+++ src/main/java/br/estudo/jms/demojms/DemoJmsApplication.java
@@ -0,0 +1,13 @@
+package br.estudo.jms.demojms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/estudo/jms/demojms/DemoJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.estudo.jms.demojms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
