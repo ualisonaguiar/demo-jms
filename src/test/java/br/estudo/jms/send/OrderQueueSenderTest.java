cd432c903d44a5f23112431592a3a3194eed5e07
Ualison Aguiar <ualison.aguiar@gmail.com>
2019-11-13 09:33:49 -0200

    pock do jms

---
+++ src/test/java/br/estudo/jms/send/OrderQueueSenderTest.java
@@ -0,0 +1,20 @@
+package br.estudo.jms.send;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderQueueSenderTest {
+
+	@Test
+	public void send() {
+
+		assertTrue(1 == 1);
+
+	}
+}
