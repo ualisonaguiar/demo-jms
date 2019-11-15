cf155f93ac58d6bbbcae183a8a14984527bb3ce0
Ualison Aguiar <ualison.aguiar@gmail.com>
2019-11-15 20:49:06 -0300

    alteracao na faixa de cep

--- src/main/java/br/estudo/jms/service/FaixaCEPService.java
+++ src/main/java/br/estudo/jms/service/FaixaCEPService.java
@@ -11,8 +11,8 @@ public class FaixaCEPService {
 		Map<String, List<Integer>> listaCEP = new HashMap<String, List<Integer>>();
 		// listaCEP.put("SP", Arrays.asList(1000000, 19999999)); // SP
 		// listaCEP.put("DF", Arrays.asList(70000000, 73699999)); // DF
-		listaCEP.put("DF", Arrays.asList(73350000, 73350700)); // DF
-		listaCEP.put("SP", Arrays.asList(12630000, 12689999)); // DF
+		listaCEP.put("DF", Arrays.asList(70000001, 72799999)); // DF
+		// listaCEP.put("SP", Arrays.asList(12630000, 12689999)); // DF
 		return listaCEP;
 	}
 }
