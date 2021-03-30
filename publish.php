0fc6bc31af93f60cf448012de441882b390bd63d
Ualison <ualison.aguiar@gmail.com>
2021-03-29 21:43:04 -0300

    fazendo integração com sonar

---
+++ publish.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ualison
+ * Date: 16/11/19
+ * Time: 23:26
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+require_once 'SendRabbitMQ.php';
+
+use Symfony\Component\Yaml\Yaml;
+
+$arrCongiYaml = Yaml::parse(file_get_contents('application.yml'));
+
+$sendRabbitMQ = new SendRabbitMQ();
+$sendRabbitMQ->send($arrCongiYaml['queue']['recebe'], '01011100');
